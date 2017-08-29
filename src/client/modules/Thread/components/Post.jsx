4c04e983bb5473521f2df3f297481f6666752d9d
Yan <jan.oasis@gmail.com>
2017-08-30 01:28:36 +0300

    Make posts styling[skip ci]

--- src/client/modules/Thread/components/Post.jsx
+++ src/client/modules/Thread/components/Post.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Link } from 'react-router';
 import Paper from 'material-ui/Paper';
 
-// import './Post.scss';
+import './Post.scss';
 
 const Post = ({ post, index }) => (
-  <Paper className="post-preview">
-    <p className="post-preview__time">{post.createdAt}</p>
-    {post.text.split('\n').map(row => (
-      <p className="post-preview__text" key={row}>
-        {row}
+  <div className="post-container">
+    <Paper className="post">
+      <p className="post__time">
+        {new Date(post.createdAt).toLocaleTimeString()}
       </p>
-    ))}
-  </Paper>
+      {post.text.split('\n').map(row => (
+        <p className="post__text" key={row}>
+          {row}
+        </p>
+      ))}
+    </Paper>
+  </div>
 );
 
 export default Post;
--- src/client/modules/Threads/components/PostPreview.jsx
+++ src/client/modules/Threads/components/PostPreview.jsx
@@ -5,18 +5,22 @@ import Paper from 'material-ui/Paper';
 import './PostPreview.scss';
 
 const PostPreview = ({ post, index }) => (
-  <Paper className="post-preview">
-    <p className="post-preview__time">{post.createdAt}</p>
-    {index === 0 ? (
-      <Link to={`${window.location.href}/${post.id}`}>Open</Link>
-    ) : null}
-    <br />
-    {post.text.split('\n').map(row => (
-      <p className="post-preview__text" key={row}>
-        {row}
+  <div className="post-preview-container">
+    <Paper className="post-preview">
+      <p className="post-preview__time">
+        {new Date(post.createdAt).toLocaleTimeString()}
       </p>
-    ))}
-  </Paper>
+      {index === 0 ? (
+        <Link to={`${window.location.href}/${post.id}`}>Open</Link>
+      ) : null}
+      <br />
+      {post.text.split('\n').map(row => (
+        <p className="post-preview__text" key={row}>
+          {row}
+        </p>
+      ))}
+    </Paper>
+  </div>
 );
 
 export default PostPreview;
