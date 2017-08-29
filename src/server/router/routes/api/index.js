7bfd28f208d1750601641274f9400d09f457bef4
Yan <jan.oasis@gmail.com>
2017-08-29 22:22:08 +0300

    Make dummy threads data[skip ci]

--- src/server/router/routes/api/index.js
+++ src/server/router/routes/api/index.js
@@ -1,7 +1,85 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send('api');
+router.get('/:boardId', (req, res) => {
+  res.send(THREADS);
 });
 
 module.exports = router;
+
+const THREADS = [
+  {
+    id: 0,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    posts: [
+      {
+        id: 0,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+      {
+        id: 1,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+      {
+        id: 2,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+    ],
+  },
+  {
+    id: 1,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    posts: [
+      {
+        id: 3,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+      {
+        id: 4,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+      {
+        id: 5,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+    ],
+  },
+  {
+    id: 2,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    posts: [
+      {
+        id: 6,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+      {
+        id: 7,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+      {
+        id: 8,
+        text: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+        sage: false,
+        createdAt: Date.now(),
+      },
+    ],
+  },
+];
