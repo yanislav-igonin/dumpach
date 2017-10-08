48720054d79d4f368db6fe2b418996d3a2990795
Yan <jan.oasis@gmail.com>
2017-10-08 03:43:06 +0300

    Install jwt and bcrypt, and make basic server endpoint

---
+++ src/server/router/routes/api/auth/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+// router.get('/:boardId', boardsHandlers.getThreads);
+
+router.post('/signin', (req, res) => console.log(req));
+module.exports = router;
--- src/server/router/routes/api/index.js
+++ src/server/router/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const boards = require('./boards');
+const auth = require('./auth');
 
 router.use('/boards', boards);
+router.use('/auth', auth);
 
 module.exports = router;
