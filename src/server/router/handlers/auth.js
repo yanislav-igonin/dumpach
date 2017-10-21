9bc497f2ef861d97b1f434cbf06a0ac537636134
Yan <jan.oasis@gmail.com>
2017-10-22 01:36:07 +0300

    Add authentication[skip ci]

--- src/server/router/handlers/auth.js
+++ src/server/router/handlers/auth.js
@@ -1,13 +1,28 @@
-// const db = require('../../db/connection/');
+const token = require('../../db/repositories/token');
+const user = require('../../db/repositories/user');
 
 module.exports = {
   async login(req, res) {
-    console.log(req);
-    res.send('ok');
+    try {
+      const userData = await user.authenticate(req.body);
+      userData.password_hash = undefined;
+      userData.created_at = undefined;
+      userData.id = undefined;
+      res.cookie('token', await token.create(userData, '1h'), {
+        httpOnly: false,
+        maxAge: 1000 * 60 * 60,
+      });
+      res.send(userData);
+    } catch (e) {
+      console.log(e.message);
+      res.status(404);
+      res.send(e.message);
+    }
   },
 
   async logout(req, res) {
     console.log(req);
+    res.clearCookie('token');
     res.send('ok');
   },
 };
