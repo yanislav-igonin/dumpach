64eff8bba81cf3eaf3b3d79c8ca33c4254a7e8cc
Yan <jan.oasis@gmail.com>
2017-10-31 20:17:21 +0300

    Return js styles[skip ci]

--- src/client/modules/Admin/modules/Dashboard.js
+++ src/client/modules/Admin/modules/Dashboard.js
@@ -1,36 +1,62 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
+import Drawer from 'material-ui/Drawer';
 import { logout } from '../duck';
 
+const styles = (theme) => ({
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+});
 
 function ButtonAppBar(props) {
+  const { classes } = props;
   return (
-    <div className='dashboard'>
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
-            className='menu-icon'
+            className={classes.menuButton}
             color="contrast"
             aria-label="Menu"
           >
             <Icon>menu</Icon>
           </IconButton>
-          <Typography type="title" color="inherit" className='dashboard__title'>
+          <Typography type="title" color="inherit" className={classes.flex}>
             Admin
           </Typography>
-          <Button color="contrast" onClick={props.logout}>Logout</Button>
+          <Button color="contrast" onClick={props.logout}>
+            Logout
+          </Button>
         </Toolbar>
       </AppBar>
+          <Drawer
+            type="persistent"
+            
+            open={true}
+          >
+
+          </Drawer>
     </div>
   );
 }
 
 const mapStateToProps = () => ({});
 
-export default connect(mapStateToProps, {logout})(ButtonAppBar);
+export default connect(mapStateToProps, { logout })(
+  withStyles(styles)(ButtonAppBar)
+);
