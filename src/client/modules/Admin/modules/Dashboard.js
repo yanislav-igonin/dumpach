702e5fe75ece7c4fa00bc511c1605885b5a4d5b7
Yan <jan.oasis@gmail.com>
2017-10-31 21:04:01 +0300

    Make drawer menu with buttons[skip ci]

--- src/client/modules/Admin/modules/Dashboard.js
+++ src/client/modules/Admin/modules/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -8,6 +9,7 @@ import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
 import Drawer from 'material-ui/Drawer';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import { logout } from '../duck';
 
 const styles = (theme) => ({
@@ -23,36 +25,68 @@ const styles = (theme) => ({
   },
 });
 
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            className={classes.menuButton}
-            color="contrast"
-            aria-label="Menu"
-          >
-            <Icon>menu</Icon>
-          </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
-            Admin
-          </Typography>
-          <Button color="contrast" onClick={props.logout}>
-            Logout
-          </Button>
-        </Toolbar>
-      </AppBar>
-          <Drawer
-            type="persistent"
-            
-            open={true}
-          >
+class ButtonAppBar extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-          </Drawer>
-    </div>
-  );
+    this.state = {
+      drawerOpened: false,
+    };
+  }
+
+  toggleDrawer = () => {
+    const { drawerOpened } = this.state;
+    this.setState({ drawerOpened: !drawerOpened });
+  };
+
+  goTo(path) {
+    browserHistory.push(`dashboard/${path}`);
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="contrast"
+              aria-label="Menu"
+              onClick={this.toggleDrawer}
+            >
+              <Icon>menu</Icon>
+            </IconButton>
+            <Typography type="title" color="inherit" className={classes.flex}>
+              Admin
+            </Typography>
+            <Button color="contrast" onClick={this.props.logout}>
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Drawer open={this.state.drawerOpened} onRequestClose={this.toggleDrawer}>
+          <List>
+            <ListItem button onClick={() => this.goTo('boards')}>
+              <ListItemIcon>
+                <Icon>inbox</Icon>
+              </ListItemIcon>
+              <ListItemText primary="Boards" />
+            </ListItem>
+            
+            <ListItem button onClick={() => this.goTo('boards')}>
+              <ListItemIcon>
+                <Icon>accessible</Icon>
+              </ListItemIcon>
+              <ListItemText primary="Users" />
+            </ListItem>
+            
+          </List>
+        </Drawer>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = () => ({});
