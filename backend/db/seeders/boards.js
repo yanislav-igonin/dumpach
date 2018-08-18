eaf20d3a21dca93a9adb24db588b1c0f1ea01986
Yanislav Igonin <jan.oasis@gmail.com>
2018-08-18 23:27:07 +0300

    Change boards seeds tities

--- backend/db/seeders/boards.js
+++ backend/db/seeders/boards.js
@@ -2,13 +2,13 @@ const boards = [
   {
     id: 1,
     identifier: 'b',
-    title: 'Random',
+    title: 'random',
     section_id: 1,
   },
   {
     id: 2,
     identifier: 'dev',
-    title: 'Development',
+    title: 'development',
     section_id: 2,
   },
 ];
