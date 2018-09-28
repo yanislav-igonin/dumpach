92975f742278c1a05c8359ae2c1016b98e376c98
Yanislav Igonin <jan.oasis@gmail.com>
2018-09-28 22:12:19 +0300

    Add threads and posts seeds

---
+++ backend/db/seeders/posts.js
@@ -0,0 +1,73 @@
+/* eslint max-len: 0 */
+const posts = [
+  {
+    title: 'Short test title',
+    text: 'Short test text',
+    is_sage: false,
+    thread_id: 1,
+  },
+  {
+    title:
+      'Average test title, test title, test title, test title, test title, test title',
+    text: 'Average test text, test text, test text, test text, test text, test text',
+    is_sage: false,
+    thread_id: 1,
+  },
+  {
+    title:
+      'Long test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title',
+    text:
+      'Average test text, test text, test text, test text, test text, test text, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title',
+    is_sage: false,
+    thread_id: 1,
+  },
+  {
+    title: 'Lorem Ipsum',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    is_sage: false,
+    thread_id: 1,
+  },
+  {
+    title: 'Short test title with sage',
+    text: 'Short test text with sage',
+    is_sage: true,
+    thread_id: 1,
+  },
+  {
+    title: 'Short test title',
+    text: 'Short test text',
+    is_sage: false,
+    thread_id: 2,
+  },
+  {
+    title:
+      'Average test title, test title, test title, test title, test title, test title',
+    text: 'Average test text, test text, test text, test text, test text, test text',
+    is_sage: false,
+    thread_id: 2,
+  },
+  {
+    title:
+      'Long test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title',
+    text:
+      'Average test text, test text, test text, test text, test text, test text, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title, test title',
+    is_sage: false,
+    thread_id: 2,
+  },
+  {
+    title: 'Lorem Ipsum',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    is_sage: false,
+    thread_id: 2,
+  },
+  {
+    title: 'Short test title with sage',
+    text: 'Short test text with sage',
+    is_sage: true,
+    thread_id: 2,
+  },
+];
+
+module.exports = posts;
---
+++ backend/db/seeders/threads.js
@@ -0,0 +1,16 @@
+const threads = [
+  {
+    board_id: 1,
+  },
+  {
+    board_id: 1,
+  },
+  {
+    board_id: 2,
+  },
+  {
+    board_id: 2,
+  },
+];
+
+module.exports = threads;
