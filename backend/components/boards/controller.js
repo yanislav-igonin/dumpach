6d888fa5eb0c63ac6ed5548287a21ae82f0005d0
Yanislav Igonin <jan.oasis@gmail.com>
2018-09-28 21:46:18 +0300

    Add error handling in boards controller

--- backend/components/boards/controller.js
+++ backend/components/boards/controller.js
@@ -2,11 +2,15 @@ const { Section, Board } = require('../../db/models');
 
 class Controller {
   static async get(ctx) {
-    const sections = await Section.findAll({
-      include: [Board],
-    });
+    try {
+      const sections = await Section.findAll({
+        include: [Board],
+      });
 
-    ctx.body = { data: sections };
+      ctx.body = { data: sections };
+    } catch (err) {
+      throw new Error(err);
+    }
   }
 }
 
