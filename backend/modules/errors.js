4ef2e9b058c271deaa7a65db7e999455149b9722
Yanislav Igonin <jan.oasis@gmail.com>
2018-09-30 18:38:00 +0300

    Update NotFound error constructor

--- backend/modules/errors.js
+++ backend/modules/errors.js
@@ -1,22 +1,20 @@
 const httpStatus = require('http-status');
 
-// TODO: think about BOOM errors
-
 class AppError extends Error {
-  constructor(message, status) {
+  constructor(message, status, error) {
     super(message);
 
     this.name = this.constructor.name;
+    this.status = status || 500;
+    this.error = error || httpStatus[500];
 
     Error.captureStackTrace(this, this.constructor);
-
-    this.status = status || 500;
   }
 }
 
 class HttpNotFoundException extends AppError {
   constructor(message) {
-    super(message || 'Not found!', httpStatus.NOT_FOUND);
+    super(message || 'Not found!', httpStatus.NOT_FOUND, httpStatus[404]);
   }
 }
 
