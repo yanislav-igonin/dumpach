d71c68fa2bc1fd31466b98cd4c3d57502bd0db7d
Yanislav Igonin <jan.oasis@gmail.com>
2018-10-01 20:55:13 +0300

    Add image resizing for thumbs

--- backend/modules/mediaFiles.js
+++ backend/modules/mediaFiles.js
@@ -1,5 +1,6 @@
 const asyncBusboy = require('async-busboy');
 const fs = require('fs-extra');
+const sharp = require('sharp');
 const uuidv4 = require('uuid/v4');
 const config = require('../config');
 
@@ -14,6 +15,7 @@ const moveFiles = async (files, board, threadId) => {
 
   await Promise.all(
     files.map(async (file) => {
+      // TODO: add try/catch
       const { size } = await fs.stat(file.path);
       const uuid = uuidv4();
       const newFileName = `${uuid}-${file.filename}`;
@@ -23,7 +25,15 @@ const moveFiles = async (files, board, threadId) => {
         type: file.mimeType,
         uuid,
       });
-      // TODO: add thumbnails creating
+
+      await fs.ensureDir(`${config.app.uploads.thumb}/${board}/${threadId}`);
+      // TODO: maybe return Promise.all
+      // TODO: maybe add size checking to not resize small images
+      await sharp(file.path)
+        .resize(200, 200)
+        .min()
+        .toFile(`${config.app.uploads.thumb}//${board}/${threadId}/${newFileName}`);
+
       return fs.move(
         file.path,
         `${config.app.uploads.source}/${board}/${threadId}/${newFileName}`,
