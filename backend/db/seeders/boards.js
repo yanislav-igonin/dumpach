9f615f79aa0783c480a62146aeb1c0267f1ea6f6
Yanislav Igonin <jan.oasis@gmail.com>
2018-10-11 19:57:07 +0300

    Update boards seeders for threads limit

--- backend/db/seeders/boards.js
+++ backend/db/seeders/boards.js
@@ -4,12 +4,14 @@ const boards = [
     identifier: 'b',
     title: 'random',
     section_id: 1,
+    threads_limit: 50,
   },
   {
     id: 2,
     identifier: 'dev',
     title: 'development',
     section_id: 2,
+    threads_limit: 50,
   },
 ];
 
