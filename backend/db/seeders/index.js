5cfc681b657722f9a30c9995acb8d81d2a9cd96d
Yanislav Igonin <jan.oasis@gmail.com>
2018-10-15 14:56:39 +0300

    Add TODO

--- backend/db/seeders/index.js
+++ backend/db/seeders/index.js
@@ -62,6 +62,7 @@ const seedPosts = async () => {
 
     /* eslint no-restricted-syntax: off */
     /* eslint no-await-in-loop: off */
+    // TODO: fix async loop seeding
     for (let thread = 1; thread <= app.seeding.threadsPerBoard; thread += 1) {
       try {
         const dbPosts = await model.findAll({ where: { thread_id: thread } });
