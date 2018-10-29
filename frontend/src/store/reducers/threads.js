467651fbf7c683c0e2bb2e823a440c40165f7bcc
Yanislav Igonin <jan.oasis@gmail.com>
2018-10-29 18:37:18 +0300

    Update threads reducer

--- frontend/src/store/reducers/threads.js
+++ frontend/src/store/reducers/threads.js
@@ -3,19 +3,24 @@ import types from '../types';
 const initialState = {
   isFetching: false,
   list: [],
+  count: 0,
+  isLastPage: true
 };
 
 const threads = (state = initialState, action) => {
   switch (action.type) {
     case types.threads.GET_THREADS:
       return {
+        ...state,
         isFetching: true,
-        list: [],
+        list: []
       };
     case types.threads.GET_THREADS_SUCCESS:
       return {
         isFetching: false,
         list: action.data,
+        count: action.count,
+        isLastPage: action.is_last_page
       };
     default:
       return state;
