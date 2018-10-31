8671888d497c96c8a3b21d1802fa76ed338f1dfc
Yanislav Igonin <jan.oasis@gmail.com>
2018-10-31 14:06:50 +0300

    Add settings reducer

---
+++ frontend/src/store/reducers/settings.js
@@ -0,0 +1,14 @@
+const initialState = {
+  threads: {
+    limitPerPage: 10
+  }
+};
+
+const settings = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default settings;
