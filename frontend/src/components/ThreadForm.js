85238cb42f2fdd7db43aa9627aad00cdbd1f307c
Yanislav Igonin <jan.oasis@gmail.com>
2018-11-02 23:32:12 +0300

    Update thread form carcass

--- frontend/src/components/ThreadForm.js
+++ frontend/src/components/ThreadForm.js
@@ -1,15 +1,69 @@
 import React, { PureComponent } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const styles = theme => ({
+  formContainer: {
+    margin: '0 10px'
+  }
+});
 
 class ThreadForm extends PureComponent {
   state = {
     title: '',
     text: '',
+    isSage: false,
     attachments: []
   };
 
   render() {
-    return <div>thread form</div>;
+    const { title, text, isSage, attachments } = this.state;
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.formContainer}>
+        <CardContent>
+          <TextField
+            id="title"
+            label="title"
+            // className={classes.textField}
+            value={title}
+            fullWidth={true}
+            // onChange={this.handleChange('name')}
+            margin="normal"
+          />
+          <TextField
+            id="text"
+            label="text"
+            // className={classes.textField}
+            value={text}
+            fullWidth={true}
+            // onChange={this.handleChange('name')}
+            margin="normal"
+          />
+          <FormGroup row>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={isSage}
+                  // onChange={this.handleChange('checkedB')}
+                  value="isSage"
+                  color="primary"
+                />
+              }
+              label="sage"
+            />
+          </FormGroup>
+        </CardContent>
+      </Card>
+    );
   }
 }
 
-export default ThreadForm;
+export default withStyles(styles)(ThreadForm);
