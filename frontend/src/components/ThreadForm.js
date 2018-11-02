33785a2b6807824fe7443a7b7e992e7dabde2c97
Yanislav Igonin <jan.oasis@gmail.com>
2018-11-02 22:51:35 +0300

    Add thread form carcass

---
+++ frontend/src/components/ThreadForm.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class ThreadForm extends PureComponent {
+  state = {
+    title: '',
+    text: '',
+    attachments: []
+  };
+
+  render() {
+    return <div>thread form</div>;
+  }
+}
+
+export default ThreadForm;
