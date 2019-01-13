d9ae12fc00ee5b66c0019e62787ffe3c2813cc23
Yanislav Igonin <jan.oasis@gmail.com>
2019-01-13 13:30:10 +0300

    Update requests api endpoint

--- frontend/src/store/sagas/boards.js
+++ frontend/src/store/sagas/boards.js
@@ -1,11 +1,12 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
+import config from '../../config';
 import types from '../types';
 
 function* getBoards() {
   try {
-    const response = yield axios.get('/api/boards');
+    const response = yield axios.get(`${config.app.api.endpoint}/boards`);
 
     if (response.status === 200) {
       yield put({ type: types.boards.GET_BOARDS_SUCCESS, data: response.data.data });
--- frontend/src/store/sagas/threads.js
+++ frontend/src/store/sagas/threads.js
@@ -1,18 +1,21 @@
 import { put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 
+import config from '../../config';
 import types from '../types';
 
 function* getThreads({ boardId, threadsPerPage, offset }) {
   try {
     const response = yield axios.get(
-      `/api/boards/${boardId}/threads?limit=${threadsPerPage}&offset=${offset}`
+      `${
+        config.app.api.endpoint
+      }/boards/${boardId}/threads?limit=${threadsPerPage}&offset=${offset}`
     );
 
     if (response.status === 200) {
       yield put({
         type: types.threads.GET_THREADS_SUCCESS,
-        data: response.data
+        data: response.data,
       });
     }
   } catch (err) {
